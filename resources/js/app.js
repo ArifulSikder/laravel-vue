5c0dab6b1fb395058ca774f6226e0785e5503aee
ArifulSikder <mdislamnoyon3@gmail.com>
2022-06-22 20:08:37 +0600

    laravel vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
-import './bootstrap';
+require('./bootstrap');
+import Vue from '~@vue';
+window.Vue = require('vue');
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import {routes} from './routes';
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+const app = new Vue({
+    el: '#app',
+    router: router,
+    data() {
+        return {
+            mounted: function(){
+                alert('hi');
+            }
+        }
+    },
+    render: h => h(App),
+});
