b03760affd50cd558676304e6cb2fb3a49f45bda
ArifulSikder <mdislamnoyon3@gmail.com>
2022-06-23 10:51:44 +0600

    dfsdf

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,26 +1,43 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
-import Vue from '~@vue';
+
 window.Vue = require('vue');
-import App from './App.vue';
-import VueRouter from 'vue-router';
-import VueAxios from 'vue-axios';
-import axios from 'axios';
-import {routes} from './routes';
-Vue.use(VueRouter);
-Vue.use(VueAxios, axios);
-const router = new VueRouter({
-    mode: 'history',
-    routes: routes
-});
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+ const routes = [
+    { path: '/home', component: Home },
+  ];
+
+
+  const router = VueRouter.createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    // history: VueRouter.createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
+// const files = require.context('./', true, /\.vue$/i);
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('home-component', require('./components/Home.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
 const app = new Vue({
     el: '#app',
-    router: router,
-    data() {
-        return {
-            mounted: function(){
-                alert('hi');
-            }
-        }
-    },
-    render: h => h(App),
+    router
 });
